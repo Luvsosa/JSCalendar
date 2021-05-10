3a96c05a461aef27203f748beacd91fe146929de
Landon <landonconstantin@gmail.com>
2021-05-10 08:19:20 -0600

    fixed UTC issue.

--- date-helpers.js
+++ date-helpers.js
@@ -1,12 +1,12 @@
 const isWeekend = day => {
     // console.log(day % 7);
 
-    // 6 for saturday, 7 for sunday
+    // 6 for saturday, 0 for sunday
     return  day % 7 === 6 || day % 7 === 0;
 }
 
 const getDayName = day => {
-    const date = new Date(Date.UTC(2018, 0, day));
+    const date = new Date(2018, 0, day);
     return new Intl.DateTimeFormat("en-US", { weekday: "short" })
     .format(date); 
 }
